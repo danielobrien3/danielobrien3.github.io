d2596085e6a0d50b2f69a600ca5b946503722767
Danny O'Brien <jbqsad@gmail.com>
2021-08-12 14:35:31 -0400

    snake can eat and grow n get points

--- js/snake.js
+++ js/snake.js
@@ -11,8 +11,12 @@ let snake = [
 	{ x: 160, y: 200 },
 ];
 
+let score = 0;
 // True if changing direction
 let changing_direction = false;
+
+let food_x;
+let food_y;
 // Horizontal velocity
 let dx = 10;
 // Vertical velocity
@@ -25,6 +29,8 @@ const snakeboard_ctx = snakeboard.getContext("2d");
 // Start game
 main();
 
+gen_food();
+
 document.addEventListener("keydown", change_direction);
 // main function called repeatedly to keep the game running
 function main() {
@@ -32,6 +38,7 @@ function main() {
 	changing_direction = false;
 	setTimeout(function onTick() {
 		clear_board();
+		drawFood();
 		move_snake();
 		drawSnake();
 		// Call main again
@@ -114,11 +121,47 @@ function drawSnake() {
 	snake.forEach(drawSnakePart);
 }
 
+function drawFood() {
+	snakeboard_ctx.fillStyle = 'lightgreen';
+	snakeboard_ctx.strokestyle = 'darkgreen';
+	snakeboard_ctx.fillRect(food_x, food_y, 10, 10);
+	snakeboard_ctx.strokeRect(food_x, food_y, 10, 10);
+}
+
+function random_food(min, max) {
+	return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+
+function gen_food() {
+	// Generate a random number the food x-coordinate
+	food_x = random_food(0, snakeboard.width - 10);
+	// Generate a random number for the food y-coordinate
+	food_y = random_food(0, snakeboard.height - 10);
+	// if the new food location is where the snake currently is, generate a new food location
+	snake.forEach(function has_snake_eaten_food(part) {
+		const has_eaten = part.x == food_x && part.y == food_y;
+		if (has_eaten) gen_food();
+	});
+}
+
 function move_snake() {
 	// Create the new Snake's head
 	const head = { x: snake[0].x + dx, y: snake[0].y + dy };
 	// Add the new head to the beginning of snake body
 	snake.unshift(head);
-	snake.pop();
+	const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
+	if (has_eaten_food) {
+		// Increase score
+		score += 10;
+
+		// !!!! DISPLAY SCORE HERE
+
+
+		// Generate new food location
+		gen_food();
+	} else {
+		// Remove the last part of snake body
+		snake.pop();
+	}
 }
 
