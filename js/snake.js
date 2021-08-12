c7a042428cf40e649c93ba13b71699b1b19c2b4a
Danny O'Brien <jbqsad@gmail.com>
2021-08-12 14:26:06 -0400

    snake can be controlled

--- js/snake.js
+++ js/snake.js
@@ -11,6 +11,8 @@ let snake = [
 	{ x: 160, y: 200 },
 ];
 
+// True if changing direction
+let changing_direction = false;
 // Horizontal velocity
 let dx = 10;
 // Vertical velocity
@@ -23,8 +25,11 @@ const snakeboard_ctx = snakeboard.getContext("2d");
 // Start game
 main();
 
+document.addEventListener("keydown", change_direction);
 // main function called repeatedly to keep the game running
 function main() {
+	if (has_game_ended()) return;
+	changing_direction = false;
 	setTimeout(function onTick() {
 		clear_board();
 		move_snake();
@@ -58,6 +63,50 @@ function drawSnakePart(snakePart) {
 	snakeboard_ctx.strokestyle = 'darkblue';
 	snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
 	snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function has_game_ended() {
+	for (let i = 4; i < snake.length; i++) {
+		if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+	}
+	const hitLeftWall = snake[0].x < 0;
+	const hitRightWall = snake[0].x > snakeboard.width - 10;
+	const hitToptWall = snake[0].y < 0;
+	const hitBottomWall = snake[0].y > snakeboard.height - 10;
+	return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall
+}
+
+function change_direction(event) {
+	const LEFT_KEY = 37;
+	const RIGHT_KEY = 39;
+	const UP_KEY = 38;
+	const DOWN_KEY = 40;
+
+	// Prevent the snake from reversing
+
+	if (changing_direction) return;
+	changing_direction = true;
+	const keyPressed = event.keyCode;
+	const goingUp = dy === -10;
+	const goingDown = dy === 10;
+	const goingRight = dx === 10;
+	const goingLeft = dx === -10;
+	if (keyPressed === LEFT_KEY && !goingRight) {
+		dx = -10;
+		dy = 0;
+	}
+	if (keyPressed === UP_KEY && !goingDown) {
+		dx = 0;
+		dy = -10;
+	}
+	if (keyPressed === RIGHT_KEY && !goingLeft) {
+		dx = 10;
+		dy = 0;
+	}
+	if (keyPressed === DOWN_KEY && !goingUp) {
+		dx = 0;
+		dy = 10;
+	}
 }
 
 /*Function that prints the parts*/
